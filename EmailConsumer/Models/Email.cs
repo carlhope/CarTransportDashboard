65578831b7b75986b9293417719950169c76cd24
Carl hope <84421131+carlhope@users.noreply.github.com>
2025-12-17 15:27:54 +0000

    refactor: move classes into separate files for clarity

---
+++ EmailConsumer/Models/Email.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailConsumer.Models
+{
+    public record Email(string RecipientUserId, EmailType EmailType, string SenderUserId);
+
+    public enum EmailType
+    {
+        JobAccepted,
+        JobAssigned,
+        PasswordReset,
+        AccountCreated,
+        Unknown
+    }
+}
