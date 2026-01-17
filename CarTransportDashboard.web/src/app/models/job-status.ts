d0cb279f99ab043f93e2fd34367e7b97eeb4c1a1
Carl hope <84421131+carlhope@users.noreply.github.com>
2026-01-17 21:25:29 +0000

    Fix Status enum to match strings returned from API

--- CarTransportDashboard.web/src/app/models/job-status.ts
+++ CarTransportDashboard.web/src/app/models/job-status.ts
@@ -1,11 +1,13 @@
 export enum JobStatus {
-  Available,
-  InProgress,
-  Completed
+  Available = 'Available',
+  InProgress = 'InProgress',
+  Completed = 'Completed',
+  Allocated = 'Allocated',
 }
 
 export const JobStatusDisplay: Record<JobStatus, string> = {
   [JobStatus.Available]: 'Available',
   [JobStatus.InProgress]: 'In Progress',
-  [JobStatus.Completed]: 'Completed'
+  [JobStatus.Completed]: 'Completed',
+  [JobStatus.Allocated]: 'Allocated',
 };
